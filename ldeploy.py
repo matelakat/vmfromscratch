3f50ee802922e2aef4b05dbc25a95cb6f8cab150
Mate Lakat <mlakat@mlakat.hu>
2014-03-02 17:45:53 +0000

    Modified deploy script

--- ldeploy.py
+++ ldeploy.py
@@ -1,7 +1,9 @@
 class RealOSOperations(object):
     def __init__(self):
         import os
+        import subprocess
         self.os = os
+        self.subprocess = subprocess
 
     def file_exists(self, filename):
         return self.os.path.exists(filename) and self.os.path.isfile(filename)
@@ -9,16 +11,36 @@ class RealOSOperations(object):
     def copy(self, source, target):
         self.files[target] = self.files[source]
 
+    def changed(self, fname):
+        """
+        >>> osops = RealOSOperations()
+        >>> osops.changed('.gitignore')
+        False
+        >>> osops.changed('nonexisting')
+        True
+        """
+        if not self.file_exists(fname):
+            return True
+        command = 'git status --porcelain -u all --'.split() + [fname]
+        proc = self.subprocess.Popen(
+            command, stdout=self.subprocess.PIPE, stderr=self.subprocess.PIPE)
+        results = [output.strip() for output in proc.communicate()]
+        return results != ['', '']
+
 
 class OSOperations(object):
     def __init__(self):
         self.files = {}
+        self.changed_gits = []
 
     def file_exists(self, filename):
         return filename in self.files
 
     def copy(self, source, target):
         self.files[target] = self.files[source]
+
+    def changed(self, fname):
+        return fname in self.changed_gits
 
 
 class VersionedConfigFile(object):
@@ -57,6 +79,44 @@ class VersionedConfigFile(object):
         True
         """
         return self._file_exists(self.saved)
+
+    def save(self):
+        """
+        If the target does not exists, nothing happens:
+        >>> cfg = VersionedConfigFile(target='target', saved='saved')
+        >>> cfg.save()
+        '[target] does not exist'
+
+        If the saved does not exist, but the target does:
+        >>> cfg.osops.files = {'target': 'some bytes'}
+        >>> cfg.save()
+        '[saved] does not exist'
+
+        If both saved and target exists:
+        >>> cfg.osops.files = {'target': 'some bytes', 'saved': 'saved bytes'}
+        >>> cfg.save()
+        '[target] -> [saved] OK'
+
+        And the contents has been overwritten:
+        >>> cfg.osops.files['saved']
+        'some bytes'
+
+        If the git status is not fine:
+        >>> cfg.osops.changed_gits = ['saved']
+        >>> cfg.save()
+        '[saved] Is out of date according to git'
+
+        """
+        if not self.target_exists():
+            return '[{0}] does not exist'.format(self.target)
+        if not self.saved_exists():
+            return '[{0}] does not exist'.format(self.saved)
+
+        if self.osops.changed(self.saved):
+            return '[{0}] Is out of date according to git'.format(self.saved)
+        self.osops.copy(self.target, self.saved)
+        return '[{0}] -> [{1}] OK'.format(self.target, self.saved)
+
 
     def restore(self):
         """
